ac4c83494984948595ff0484a1603ded88171baf
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-01-31 12:11:46 -0500

    s2c1 Début de sass

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
 /**
 * index.php ets le modèle par défaut du thème 4w4
- */?>
+ */
+?>
 <?php get_header();/*** va aller chercher le fichier header.php */?>
     <main>
     <h1>Bienvenue sur 4W4</h1>
