1bd7631b39dd146b918dc1e793d260fe79c9dc0b
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-02-02 11:48:56 -0500

    s2c2 Ajout de front-page.php et single.php

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>mon 4W4</title>
+    <a href="<?= bloginfo('description');?>"><h1><?= bloginfo('name'); ?></h1></a> 
+    <h2><?= bloginfo('url'); ?></h2>
     <?php wp_head() /**sa va intégrer lentete de notre page.  */ ?> 
 </head>
 <body>
--- single.php
+++ single.php
@@ -1,18 +1,17 @@
 <?php 
 /**
-* front-page.php ets le modèle par défaut du thème 4w4
+* single.php ets le modèle par défaut qui affiche un article
  */
 ?>
 <?php get_header();/*** va aller chercher le fichier header.php */?>
     <main>
-        <pre>front-page.php</pre>
+        <pre>single.php</pre>
         <h1>Bienvenue sur 4W4</h1>
     <? if(have_posts()):
     while(have_posts()): the_post(); ?>
         <article>
-            <a href="<?php the_permalink(); ?> "><h3><?php the_title(); ?></h3></a>
-            <?php the_excerpt(); ?>
-            <?php the_permalink(); ?> 
+            <h3><?php the_title();?></h3>
+            <?php the_content();?> 
         </article>
         <hr>
     <?php endwhile;?>
