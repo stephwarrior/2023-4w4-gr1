48cbbeee5d400a656db32f2a0513c3d8e514eb9f
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-02-14 11:43:27 -0500

    s4c1 front-page.php wp_trim_words() markdown.scss, .blocflex

--- front-page.php
+++ front-page.php
@@ -6,18 +6,19 @@
 
 <?php get_header(); ?>
     <main>
-        <pre>front-page.php</pre>
-        <h1>Bienvenue sur 4W4</h1>
-    <?php if(have_posts()):
-            while (have_posts()): the_post(); ?>
-            <article>
-                <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
-                <?php the_excerpt(); ?>
-                <?php the_permalink(); ?>
-            </article>
-            <hr>
-            <?php endwhile; ?>
-           <?php  endif; ?>
+            <pre>front-page.php</pre>
+            <h1>Bienvenue sur 4W4</h1>
+        <section class="blocflex">
+            <?php if(have_posts()):
+                while (have_posts()): the_post(); ?>
+                <article>
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <hr>
+                    <?= wp_trim_words(get_the_excerpt(),10,"...") ?> 
+                </article>
+                <?php endwhile; ?>
+            <?php  endif; ?>
+        </section>
     </main>
     
     <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -10,12 +10,14 @@
 </head>
 <body>
     <header class="site__entete">
+        <section class="entete__nav">
         <?php the_custom_logo(); ?> 
         <?php wp_nav_menu(array(
                         "menu"=> "entete",
                         "container" => "nav",
                         "container_class" => "menu__entete"
         )); ?>   
-    <h1><a class="site__titre" href="<?= bloginfo('url');?>"><?= bloginfo('name'); ?></a></h1> 
-    <h2><?= bloginfo('description'); ?></h2>
+        </section>
+    <h1 class="site__titre"><a href="<?= bloginfo('url');?>"><?= bloginfo('name'); ?></a></h1> 
+    <h2 class="site__description"><?= bloginfo('description'); ?></h2>
     </header>
