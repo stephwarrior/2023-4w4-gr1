2767a61d6ea6f4d3d3fdadd0673c5536428c38ee
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-02-21 09:29:33 -0500

    début sccs pour list markdown - category.php

--- category.php
+++ category.php
@@ -1,21 +1,21 @@
 <?php 
 /**
- * category.php est le modèle par défaut 
- * pour afficher une archive d'article de catégorie spécifique 
+ * category.php est le modèle par défaut pour 
+ * afficher une archive d'articles de catégorie spécifique 
  */
 ?>
 
 <?php get_header(); ?>
     <main>
-            <pre>category.php</pre>
-            <h1>Bienvenue sur 4W4</h1>
+        <pre>category.php</pre>
+        <h1>Bienvenue sur 4W4</h1>
         <section class="blocflex">
             <?php if(have_posts()):
                 while (have_posts()): the_post(); ?>
                 <article>
                     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                     <hr>
-                    <?= wp_trim_words(get_the_excerpt(),10,"...") ?> 
+                    <?= wp_trim_words(get_the_excerpt(), 10, " ... "); ?>
                 </article>
                 <?php endwhile; ?>
             <?php  endif; ?>
